656230bc79d4b27b59d2e337708b41279a816e2a
Alexis <Alexismatamoros1996888@gmail.com>
2020-09-23 22:22:53 -0600

    Creacion Compoente Sidebar
    Usar Grilla

--- Frontend/ownshop/src/app/app-routing.module.ts
+++ Frontend/ownshop/src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { LandingComponent } from 'src/app/pages/landing/landing.component';
 import { LoginComponent} from 'src/app/pages/auth/login/login.component'
 import { RegisterComponent} from 'src/app/pages/auth/register/register.component'
 import { RegisterEnterpriseComponent} from 'src/app/pages/auth/register-enterprise/register-enterprise.component'
+import { MainComponent } from 'src/app/pages/main/main.component'
+import { SidebarComponent } from 'src/app/pages/shared/sidebar/sidebar.component'
 //rutas
 const routes: Routes = [
   {"path":"",component:LandingComponent},
   {"path":"login",component:LoginComponent},
   {"path":"register",component:RegisterComponent},
   {"path":"registerEnterprise",component:RegisterEnterpriseComponent},
+  {'path':"main",component:MainComponent}
+
+
 ];
 
 @NgModule({
--- Frontend/ownshop/src/app/app.module.ts
+++ Frontend/ownshop/src/app/app.module.ts
@@ -4,19 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { RegisterEnterpriseComponent } from './pages/auth/register-enterprise/register-enterprise.component';
+import { SidebarComponent } from './pages/shared/sidebar/sidebar.component';
+import { MainComponent } from './pages/main/main.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    NavbarComponent,
     LoginComponent,
     RegisterComponent,
     RegisterEnterpriseComponent,
+    SidebarComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
