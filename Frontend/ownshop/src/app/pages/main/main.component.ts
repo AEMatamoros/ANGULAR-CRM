caf5dcc4c982aa41eecb9d092c2740961d9c7baf
Alexis <Alexismatamoros1996888@gmail.com>
2020-09-24 20:21:57 -0600

    PAgina principal de inicio para empresa completa

--- Frontend/ownshop/src/app/pages/main/main.component.ts
+++ Frontend/ownshop/src/app/pages/main/main.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  stores:number[]=[1,2,3,4,5,6]
+  data:boolean=false
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeData(){
+    if (this.data==false){
+      this.data=true
+    }else{
+      this.data=false
+    }
+    
+  }
 }
