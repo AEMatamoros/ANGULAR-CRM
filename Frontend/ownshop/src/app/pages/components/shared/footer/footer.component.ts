fcc27255229016a9ae1ecaffe18e8bb14b233b53
Alexis <Alexismatamoros1996888@gmail.com>
2020-10-02 21:14:15 -0600

    Responsive footer

--- Frontend/ownshop/src/app/pages/components/shared/footer/footer.component.ts
+++ Frontend/ownshop/src/app/pages/components/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  currentRoute:string=''
+  dataChange:boolean
+  constructor(private router: Router) {
+    this.router.events.subscribe((ev) => {
+      if (ev instanceof NavigationEnd) { 
+        this.currentRoute=this.router.url;
+        //console.log(this.currentRoute)
+        if(this.currentRoute=='/' || this.currentRoute=='/login' || this.currentRoute=='/register'|| this.currentRoute=='/registerEnterprise'){
+          this.dataChange=true
+        }else{
+          this.dataChange=false
+        }
+      }
+    });}
 
   ngOnInit(): void {
   }
 
+  
 }
