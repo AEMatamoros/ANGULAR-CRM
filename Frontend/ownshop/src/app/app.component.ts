0002d85d7ad96fcc652b8fb5500ff6e7b842c0bc
Alexis <Alexismatamoros1996888@gmail.com>
2020-10-28 16:49:27 -0600

    Se incorporo un tag para agregar los productos a la plantilla.

--- Frontend/ownshop/src/app/app.component.ts
+++ Frontend/ownshop/src/app/app.component.ts
@@ -24,10 +24,12 @@ export class AppComponent  {
       if (ev instanceof NavigationEnd) { 
         this.currentRoute=this.router.url;
         //console.log(this.currentRoute)
-        if(this.currentRoute=='/' || this.currentRoute=='/login' || this.currentRoute=='/register'|| this.currentRoute=='/registerEnterprise'){
+        if(this.currentRoute=='/' || this.currentRoute=='/login' || this.currentRoute=='/register'|| this.currentRoute=='/registerEnterprise' || this.currentRoute.includes('/templatepreview')){
           this.showNav=false
+          console.log(this.currentRoute)
         }else{
           this.showNav=true
+          console.log(this.currentRoute)
         }
       }
 
--- Frontend/ownshop/src/app/pages/components/admin/templates/templatepreview/templatepreview.component.ts
+++ Frontend/ownshop/src/app/pages/components/admin/templates/templatepreview/templatepreview.component.ts
@@ -11,29 +11,44 @@ import { TemplatesService } from 'src/app/services/templates/templates.service'
 export class TemplatepreviewComponent implements OnInit {
   tempId:string=''
   template:any
+  tempManage:any
+  isProductTemp:boolean
 
   constructor(private router:Router,private templateService:TemplatesService, private route:ActivatedRoute) { }
   
-  html:string=`
-  <center>
-  <h1>My Pagina Web</h1>
-  <p>Data</p>
-  <a class="btn btn-success">Boton</a>
-  </center>
-  `
-  css:any={
-    color:'black', 
-  }
+  html:string=``
+  html2:string=``
+  admin:boolean=true
+  productTemp:string=`<div class='container alert-success'><div class="row ">
+                      <ng-container *ngFor="let store of products">
+                      <div class="card m-2 col-md-4 col-lg-3 col-xl-3 col-sm-12 col-xs-12 " style="width: 100%;">
+                      <img class="card-img-top" src="assets/img/cover.jpg" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">OwnShop Product</h5>
+                          <p class="card-text">Descripcion del Producto <strong>$999</strong></p>
+
+                          <a [routerLink]="['/editstore']" class="btn btn-warning m-1">Comprar</a>
+                        </div>
+                      </div>
+                    </ng-container>
+                    </div>
+                    </div>`
+  
   products:number[]=[1,2,3]
   //classes:string='alert-success'
-  classes:string=''
   ngOnInit(): void {
     this.route.params.subscribe(params=>{
         this.tempId=params['id']
         this.templateService.getTemplate(this.tempId).subscribe(res=>{
           this.template=res
-          this.html=this.template.html
-          this.css=JSON.parse(JSON.stringify({"color":'red'})) 
+          if(this.template.html.indexOf('<products>') > -1){
+            this.isProductTemp=true
+            this.tempManage=this.template.html.split('<products>');
+            this.html=`<head><style>${this.template.css}</style></head>`+this.tempManage[0];
+            this.html2=this.tempManage[1]
+          }else{
+            this.html=`<head><style>${this.template.css}</style></head>`+this.template.html
+          }
         },
         err=>(console.log(err))
         )
