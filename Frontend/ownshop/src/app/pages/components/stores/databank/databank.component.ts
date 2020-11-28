9bee55ee4629f790ef6a41980c785cfb6ec4896f
Alexis <Alexismatamoros1996888@gmail.com>
2020-11-27 18:00:57 -0600

    Se modifico el Banco de datos, get yu post listos, se debe agregar delete

--- Frontend/ownshop/src/app/pages/components/stores/databank/databank.component.ts
+++ Frontend/ownshop/src/app/pages/components/stores/databank/databank.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabankService } from 'src/app/services/databank/databank.service'
+import { ProductService } from 'src/app/services/product/product.service'
+import { StoreService } from 'src/app/services/store/store.service'
 import { Router,ActivatedRoute } from '@angular/router'
 import { subscribeOn } from 'rxjs/operators';
 import { FormControl,FormGroup,Validators } from '@angular/forms'
@@ -10,10 +12,12 @@ import { FormControl,FormGroup,Validators } from '@angular/forms'
   styleUrls: ['./databank.component.css']
 })
 export class DatabankComponent implements OnInit {
+  //Initializers
   myFile
   segmentedFileName
   extension
   storeId
+  store
   data={type:'',route:'',store:'',parent:''}
   images
   videos
@@ -24,21 +28,28 @@ export class DatabankComponent implements OnInit {
   isFolder:boolean=false
   currentFolder=""
   parent=""
+  folderCount
+  folderOrder=[]
+  products
+  //Shortcuts
+
   newFolderForm= new FormGroup({
     folderName: new FormControl('',Validators.required),
     parentFolder: new FormControl('')
   })
-  constructor(private databankService:DatabankService,private route:ActivatedRoute) { }
+  constructor(private databankService:DatabankService,private route:ActivatedRoute,private productService:ProductService,private storeService:StoreService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params=>{
       this.storeId=params['id']
-      this.databankService.getBankImgs(this.storeId+'/none').subscribe(res=>this.images=res)
-      this.databankService.getBankVids(this.storeId+'/none').subscribe(res=>this.videos=res)
-      this.databankService.getBankPdf(this.storeId+'/none').subscribe(res=>this.pdfs=res)
-      this.databankService.getBankRar(this.storeId+'/none').subscribe(res=>this.rars=res)
-      this.databankService.getBankOther(this.storeId+'/none').subscribe(res=>this.others=res)
+      this.databankService.getBankImgs(this.storeId+'/none').subscribe(res=>{this.images=res,this.addShortCutImages(this.images);})
+      this.databankService.getBankVids(this.storeId+'/none').subscribe(res=>{this.videos=res,this.addShortCutVideos(this.videos)})
+      this.databankService.getBankPdf(this.storeId+'/none').subscribe(res=>{this.pdfs=res,this.addShortCutPdfs(this.pdfs)})
+      this.databankService.getBankRar(this.storeId+'/none').subscribe(res=>{this.rars=res,this.addShortCutRars(this.rars)})
+      this.databankService.getBankOther(this.storeId+'/none').subscribe(res=>{this.others=res,this.addShortCutOthers(this.others)})
       this.databankService.getBankFolders(this.storeId,'none').subscribe(res=>this.folders=res);
+      this.productService.getStoreProducts(this.storeId).subscribe(res=>{this.products=res,this.addShortCutProducts(this.products)})
+      this.storeService.getStore(this.storeId).subscribe(res=>this.store=res)
     })
   }
 
@@ -65,7 +76,7 @@ export class DatabankComponent implements OnInit {
       case 'pdf':
         this.data.type="pdf"
         break;
-      case 'rar':
+      case 'rar' || 'zip':
         this.data.type="rar"
         break;
       default:
@@ -91,15 +102,71 @@ export class DatabankComponent implements OnInit {
 
   openFolder(folderId){
       this.callData(folderId)
+      this.isFolder=true
+      this.folderOrder.push(folderId)
+      //console.log(this.folderOrder)
+  }
+
+  goBack(){
+    this.folderCount=0
+    console.log(this.folderOrder)
+    if(this.folderOrder.length>0){
+      this.currentFolder=this.folderOrder.pop()
+      this.callData(this.currentFolder)
+      //console.log(this.folderOrder.length)
+
+    }else{
+      this.currentFolder=this.folderOrder.pop()
+      this.ngOnInit()
+    }
+    
   }
 
   callData(folderId){
-    this.databankService.getBankImgs(this.storeId+`/${folderId}`).subscribe(res=>{this.images=res,console.log(res)})
+    this.databankService.getBankImgs(this.storeId+`/${folderId}`).subscribe(res=>{this.images=res})
       this.databankService.getBankVids(this.storeId+`/${folderId}`).subscribe(res=>this.videos=res)
       this.databankService.getBankPdf(this.storeId+`/${folderId}`).subscribe(res=>this.pdfs=res)
       this.databankService.getBankRar(this.storeId+`/${folderId}`).subscribe(res=>this.rars=res)
       this.databankService.getBankOther(this.storeId+`/${folderId}`).subscribe(res=>this.others=res)
       this.databankService.getBankFolders(this.storeId,folderId).subscribe(res=>this.folders=res);
       this.currentFolder=folderId
+      
   }
+
+  addShortCutImages(images){
+    images.forEach(img => {
+      img['shortcut']=`{imagen:${img['_id']}}`
+    });
+  }
+
+  addShortCutVideos(videos){
+    videos.forEach(vid => {
+      vid['shortcut']=`{video:${vid['_id']}}`
+    });
+  }
+
+  addShortCutPdfs(pdfs){
+    pdfs.forEach(pdf => {
+      pdf['shortcut']=`{pdf:${pdf['_id']}}`
+    });
+  }
+
+  addShortCutRars(rars){
+    rars.forEach(rar => {
+      rar['shortcut']=`{rar:${rar['_id']}}`
+    });
+  }
+
+  addShortCutOthers(others){
+    others.forEach(other => {
+      other['shortcut']=`{other:${other['_id']}}`
+    });
+  }
+
+  addShortCutProducts(products){
+    products.forEach(product => {
+      product['shortcut']=`{product:${product['_id']}}`
+    });
+  }
+  
 }
